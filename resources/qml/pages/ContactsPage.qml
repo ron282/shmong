ron282/harbour-shmoose

ui improvements: messaging page, availability status, contacts

resources/qml/pages/ContactsPage.qml
@@ -6,25 +6,23 @@ Page {
     id: page;
     allowedOrientations: Orientation.All;
 
-//    Component {
-//        id: sectionHeading
-//        Rectangle {
-//            width: container.width
-//            height: childrenRect.height
-//
-//            required property string section
-//
-//            Label {
-//                text: parent.section
-//                font.pixelSize: Theme.fontSizeLarge
-//            }
-//        }
-//    }
+    Component {
+        id: sectionHeading
+
+        Label {
+            text: section
+            color: Theme.highlightColor
+            font.pixelSize: Theme.fontSizeLarge
+            anchors {
+                left: parent.left
+                leftMargin: Theme.paddingMedium
+            }
+        }
+    }
 
     SilicaListView {
         id: jidlist
         header: Column {
-            spacing: Theme.paddingMedium;
             anchors {
                 left: parent.left;
                 right: parent.right;
@@ -42,11 +40,31 @@ Page {
             //            }
         }
         model: shmoose.rosterController.rosterList
-        spacing: Theme.paddingLarge
+        spacing: Theme.paddingMedium;
+
+        section.property: "name"
+        section.delegate: sectionHeading
+        section.criteria: ViewSection.FirstCharacter
 
         delegate: ListItem {
             id: item;
-            menu: contextMenu
+
+            menu: ContextMenu {
+                MenuItem {
+                    text:  qsTr("Remove");
+                    onClicked: {
+                        remorseAction(qsTr("Remove contact"),
+                        function() {
+                            if (shmoose.rosterController.isGroup(jid)) {
+                                shmoose.removeRoom(jid);
+                            }
+                            else {
+                                shmoose.rosterController.removeContact(jid);
+                            }
+                        });
+                    }
+                }
+            }
             contentHeight: Theme.itemSizeMedium;
             onClicked: {
                 shmoose.setCurrentChatPartner(jid)
@@ -60,6 +78,7 @@ Page {
                 anchors {
                     top: parent.top;
                     left: parent.left;
+                    leftMargin: Theme.paddingMedium
                     bottom: parent.bottom;
                 }
 
@@ -70,9 +89,19 @@ Page {
                     anchors.fill: parent;
                 }
             }
+            Rectangle {
+                id: presence
+                anchors {
+                    left: img.right
+                    top: img.top
+                }
+                width: Theme.paddingSmall
+                height: img.height
+                color: availability === RosterItem.AVAILABILITY_ONLINE ? "lime" : availability === RosterItem.AVAILABILITY_OFFLINE ? "gray" : "transparent"
+            }
             Column {
                 anchors {
-                    left: img.right;
+                    left: presence.right;
                     right: parent.right
                     margins: Theme.paddingMedium;
                     verticalCenter: parent.verticalCenter;
@@ -89,27 +118,6 @@ Page {
                 Row {
                     width: parent.width
 
-                    Rectangle {
-                        id: presence
-                        anchors {
-                            bottom: img.bottom
-                            right: img.right
-                            bottomMargin: Theme.paddingSmall
-                            rightMargin: Theme.paddingSmall
-                        }
-                        width: Math.max(lbl.implicitWidth+radius, Theme.iconSizeSmall)
-                        height: Theme.iconSizeExtraSmall
-                        radius: height*0.5
-                        color: availability === RosterItem.AVAILABILITY_ONLINE ? "green" : "gray"
-                        Label {
-                            id: lbl
-                            font.bold: true
-                            text: availability !== RosterItem.AVAILABILITY_ONLINE && availability !== RosterItem.AVAILABILITY_OFFLINE ? "?" : ""
-                            font.pixelSize: Theme.fontSizeTiny
-                            anchors.horizontalCenter: parent.horizontalCenter
-                            anchors.verticalCenter: parent.verticalCenter
-                        }
-                    }
                     Image {
                         id: subscriptionImage;
                         visible: ! shmoose.rosterController.isGroup(jid)
@@ -123,7 +131,7 @@ Page {
                     Label {
                         id: jidId;
                         text: jid;
-                        width: parent.width - subscriptionImage.width - presence.width - 2*Theme.paddingMedium
+                        width: parent.width - subscriptionImage.width - Theme.paddingMedium
                         truncationMode: TruncationMode.Fade
                         color: Theme.secondaryColor;
                         font.pixelSize: Theme.fontSizeTiny;
@@ -134,25 +142,6 @@ Page {
                     text: status;
                     color: Theme.secondaryColor;
                     font.pixelSize: Theme.fontSizeTiny;
-                }
-                Component {
-                    id: contextMenu
-                    ContextMenu {
-                        MenuItem {
-                            text:  qsTr("Remove");
-                            onClicked: {
-                                remorseAction(qsTr("Remove contact"),
-                                function() {
-                                    if (shmoose.rosterController.isGroup(jid)) {
-                                        shmoose.removeRoom(jid)
-                                    }
-                                    else {
-                                        shmoose.rosterController.removeContact(jid)
-                                    }
-                                })  
-                            }
-                        }
-                    }
                 }
             }
         }
@@ -186,9 +175,6 @@ Page {
 
         }
 
-//        section.property: "name"
-//        section.criteria: ViewSection.FirstCharacter
-//        section.delegate: sectionHeading
     }
 
     function getImage(jid) {

resources/qml/pages/ConversationsPage.qml
@@ -29,6 +29,7 @@ Page {
         delegate: ListItem {
             id: item;
             contentHeight: Theme.itemSizeMedium;
+            property int availability : shmoose.rosterController.getAvailability(jid)
 
             onClicked: {
                 console.log("set current char partner: " + jid);
@@ -54,10 +55,20 @@ Page {
                 }
             }
             Rectangle {
+                id: presence
+                anchors {
+                    left: img.right
+                    top: img.top
+                }
+                width: Theme.paddingSmall
+                height: img.height
+                color: availability === RosterItem.AVAILABILITY_ONLINE ? "lime" : availability === RosterItem.AVAILABILITY_OFFLINE ? "gray" : "transparent"
+            }
+            Rectangle {
                 width: Math.max(lblUnread.implicitWidth+radius, height)
                 height: lblUnread.implicitHeight
                 color: Theme.highlightBackgroundColor
-                radius: width*0.5
+                radius: height*0.5
                 anchors {
                     top: img.top
                     right: img.right
@@ -76,7 +87,7 @@ Page {
             }
             Column {
                 anchors {
-                    left: img.right;
+                    left: presence.right;
                     margins: Theme.paddingMedium;
                     verticalCenter: parent.verticalCenter;
                     right: parent.right
@@ -129,6 +140,12 @@ Page {
                                             shmoose.persistence.removeConversation(jid);
                                         })
                     }
+                }
+            }
+            Connections {
+                target: shmoose
+                onRosterListChanged: {
+                    availability = shmoose.rosterController.getAvailability(jid);
                 }
             }
         }

ron282/shmong

resources/qml/pages/ContactsPage.qml
@@ -6,10 +6,23 @@ Page {
     id: page;
     allowedOrientations: Orientation.All;
 
+    Component {
+        id: sectionHeading
+
+        Label {
+            text: section
+            color: Theme.highlightColor
+            font.pixelSize: Theme.fontSizeLarge
+            anchors {
+                left: parent.left
+                leftMargin: Theme.paddingMedium
+            }
+        }
+    }
+
     SilicaListView {
         id: jidlist
         header: Column {
-            spacing: Theme.paddingMedium;
             anchors {
                 left: parent.left;
                 right: parent.right;
@@ -27,9 +40,31 @@ Page {
             //            }
         }
         model: shmong.rosterController.rosterList
+        spacing: Theme.paddingMedium;
+
+        section.property: "name"
+        section.delegate: sectionHeading
+        section.criteria: ViewSection.FirstCharacter
+
         delegate: ListItem {
             id: item;
-            menu: contextMenu
+
+            menu: ContextMenu {
+                MenuItem {
+                    text:  qsTr("Remove");
+                    onClicked: {
+                        remorseAction(qsTr("Remove contact"),
+                        function() {
+                            if (shmong.rosterController.isGroup(jid)) {
+                                shmong.removeRoom(jid);
+                            }
+                            else {
+                                shmong.rosterController.removeContact(jid);
+                            }
+                        });
+                    }
+                }
+            }
             contentHeight: Theme.itemSizeMedium;
 
             function removeContact() {
@@ -56,6 +91,7 @@ Page {
                 anchors {
                     top: parent.top;
                     left: parent.left;
+                    leftMargin: Theme.paddingMedium
                     bottom: parent.bottom;
                 }
 
@@ -66,9 +102,20 @@ Page {
                     anchors.fill: parent;
                 }
             }
+            Rectangle {
+                id: presence
+                anchors {
+                    left: img.right
+                    top: img.top
+                }
+                width: Theme.paddingSmall
+                height: img.height
+                color: availability === RosterItem.AVAILABILITY_ONLINE ? "lime" : availability === RosterItem.AVAILABILITY_OFFLINE ? "gray" : "transparent"
+            }
             Column {
                 anchors {
-                    left: img.right;
+                    left: presence.right;
+                    right: parent.right
                     margins: Theme.paddingMedium;
                     verticalCenter: parent.verticalCenter;
                 }
@@ -82,6 +129,8 @@ Page {
                     font.pixelSize: Theme.fontSizeMedium;
                 }
                 Row {
+                    width: parent.width
+
                     Image {
                         id: subscriptionImage;
                         visible: ! shmong.rosterController.isGroup(jid)
@@ -94,6 +143,8 @@ Page {
                     Label {
                         id: jidId;
                         text: jid;
+                        width: parent.width - subscriptionImage.width - Theme.paddingMedium
+                        truncationMode: TruncationMode.Fade
                         color: Theme.secondaryColor;
                         font.pixelSize: Theme.fontSizeTiny;
                     }

resources/qml/pages/ConversationsPage.qml
@@ -28,6 +28,7 @@ Page {
         delegate: ListItem {
             id: item;
             contentHeight: Theme.itemSizeMedium;
+            property int availability : shmong.rosterController.getAvailability(jid)
 
             onClicked: {
                 console.log("set current char partner: " + jid);
@@ -52,9 +53,40 @@ Page {
                     anchors.fill: parent;
                 }
             }
+            Rectangle {
+                id: presence
+                anchors {
+                    left: img.right
+                    top: img.top
+                }
+                width: Theme.paddingSmall
+                height: img.height
+                color: availability === RosterItem.AVAILABILITY_ONLINE ? "lime" : availability === RosterItem.AVAILABILITY_OFFLINE ? "gray" : "transparent"
+            }
+            Rectangle {
+                width: Math.max(lblUnread.implicitWidth+radius, height)
+                height: lblUnread.implicitHeight
+                color: Theme.highlightBackgroundColor
+                radius: height*0.5
+                anchors {
+                    top: img.top
+                    right: img.right
+                    topMargin: Theme.paddingSmall
+                    rightMargin: Theme.paddingSmall
+                }
+                visible: (unreadmessages > 0) ? true : false
+                Label {
+                    id: lblUnread
+                    font.bold: true
+                    text: unreadmessages
+                    font.pixelSize: Theme.fontSizeTiny
+                    anchors.horizontalCenter: parent.horizontalCenter
+                    anchors.verticalCenter: parent.verticalCenter
+                }
+            }
             Column {
                 anchors {
-                    left: img.right;
+                    left: presence.right;
                     margins: Theme.paddingMedium;
                     verticalCenter: parent.verticalCenter;
                     right: parent.right
@@ -112,6 +144,12 @@ Page {
                                             shmong.persistence.removeConversation(jid);
                                         })
                     }
+                }
+            }
+            Connections {
+                target: shmong
+                onRosterListChanged: {
+                    availability = shmong.rosterController.getAvailability(jid);
                 }
             }
         }